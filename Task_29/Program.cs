dffac1ec7ab37639418e77f776eab4509b090377
Ekaterina <alexeitseva.katya@yandex.ru>
2022-11-10 20:27:46 +0700

    Task_29

---
+++ Task_29/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int[] arr = { 4, 5, 7, 6, 1, 8, 2, 9 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(arr);
